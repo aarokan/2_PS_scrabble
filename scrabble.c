094b5d0baebf4ce5b0f23d638f795af9b6056e59
AR <aarokan@gmail.com>
2024-01-27 22:02:01 +0300

    3_scrabble initial commit

---
+++ scrabble.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Prompt the user for two words
+
+    // Compute the score of each word
+
+    // Print the winner
+}
