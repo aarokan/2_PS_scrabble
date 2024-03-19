a552ea172c457f1d7d49416d642a351df2f0b074
AR <aarokan@gmail.com>
2024-03-19 03:16:11 +0300

    create compute_score function

--- scrabble.c
+++ scrabble.c
@@ -3,11 +3,22 @@
 #include <stdio.h>
 #include <string.h>
 
+int compute_score(string word);
+
 int main(void)
 {
     // Prompt the user for two words
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
 
     // Compute the score of each word
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
 
     // Print the winner
+}
+
+int compute_score(string word)
+{
+    // compute and return score for word
 }
